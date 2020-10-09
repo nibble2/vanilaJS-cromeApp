b16b8ea45c2eb96d0463955f2ebbb3bc7b40747e
nibble2 <nibblekk@gmail.com>
2020-10-10 07:57:20 +0900

    Add askForName, inputSubmit, saveLocalStorage function

--- gretting.js
+++ gretting.js
@@ -5,18 +5,35 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    console.log(currentValue)
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    console.log("test1");
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
 
-
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     console.log(currentUser);
     if (currentUser === null) {
-        // she is not
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
