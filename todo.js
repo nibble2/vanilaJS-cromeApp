be94658af32c53c6a5f90c0b4a674da1785fceab
nibble2 <nibblekk@gmail.com>
2020-10-13 21:27:46 +0900

    Add paintToDo function

--- todo.js
+++ todo.js
@@ -5,13 +5,22 @@ const toDoList = document.querySelector(".js-toDoList");
 const TODOS = 'toDos';
 
 function paintToDo(text) {
-    console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "❌";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+
 }
 
 function handleSubmit(event) {
     event.preventDefault();
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
+    toDoInput.value = "";
 }
 
 function loadToDos() {
